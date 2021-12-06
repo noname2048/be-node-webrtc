c34b83fbae1f459ea69ffef96f7ec2916d1c751d
noname2048 <sungwook.csw@gmail.com>
2021-12-06 21:44:22 +0900

    fix: fix nodemon, babel, css, pug files

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ import SocketIo from "socket.io";
 
 const app = express();
 
-app.set("view engin", "pug");
+app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("home"));
